4ed6d9eda4b11b52471d84d526a1f7a5e16403eb
Sebastian-Schmitz <sebastian.schmitz3@rwth-aachen.de>
2014-10-25 16:41:22 +0200

    Add first files (testing git in eclipse)

---
+++ ProjectEuler/src/Challenge.java
@@ -0,0 +1,13 @@
+
+public class Challenge {
+	
+	public String name;
+	
+	public Challenge(String name){
+		this.name = name;
+	}
+	
+	public void printSolution(){
+		System.out.println("solution goes here");
+	}
+}
--- ProjectEuler/src/ProjectEuler.java
+++ ProjectEuler/src/ProjectEuler.java
@@ -4,5 +4,9 @@ public class ProjectEuler {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo.");
+		
+		Challenge challenge = new Challenge("number1");
+		challenge.printSolution();
+		
 	}
 }
