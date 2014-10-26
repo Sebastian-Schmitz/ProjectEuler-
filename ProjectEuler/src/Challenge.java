e50773e1a7f2fcd8c540f769b3630b0f81255f75
Sebastian Schmitz <muellhier@posteo.de>
2014-10-26 09:47:12 +0100

    started adding prob44

--- ProjectEuler/src/Challenge.java
+++ ProjectEuler/src/Challenge.java
@@ -2,9 +2,16 @@
 public class Challenge {
 	
 	public String name;
+	public int number;
+	public String problem;
 	
-	public Challenge(String name){
-		this.name = name;
+	public Challenge(int number, String problem){
+		this.number = number;
+		this.problem = problem;
+	}
+	
+	public void printProblem(){
+		System.out.println(problem);
 	}
 	
 	public void printSolution(){
@@ -12,6 +19,10 @@ public class Challenge {
 	}
 	
 	public void problemx(){
+		
+		3n*n-n/2
+		
+		
 		
 		System.out.println("solution");
 		System.out.println("solution");
